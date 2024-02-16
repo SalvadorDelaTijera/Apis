cc3bbd95eafb98428aee54f59ac022e4d8b4bda3
Salvador De la T <stijera@MacBook-Pro-de-stijera.local>
2024-02-15 20:03:29 -0600

    comit 8:03 pm 15-02-2024 / props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,46 @@
 import React from 'react'
 import Datos from './Datos/Datos'
+import { useState, useEffect } from 'react';
 
 const App = () => {
+
+  const [dataApi, setDataApi] = useState();
+
+  const handlerData = async () => {
+    let resp = await fetch("https://api.publicapis.org/entries");
+    resp = await resp.json();
+    setDataApi(resp);
+  };
+
+  useEffect(() => {
+    handlerData();
+  }, []);
+
+  //console.log(dataApi);
   return (
     <>
+     <h1>Apis info</h1>
+     <Datos name ='Api' cat = 'categoria' desc= 'descripcion' link = 'link' />
 
-    <Datos/>
+      <div>
+      {
+        dataApi?.entries?.map((API)=>{
+         return(
+          <ul >
+          <h3>{API.API}</h3>
+         <li>categoria: {API.Category}</li>
+         <li>descripción: {API.Description}</li>
+         <a href="">{API.Link}</a>
+        </ul>
+         )
+        })
+      }
+      
+      </div>
+
     </>
   )
 }
 
 export default App
+    //<Datos name = {API.API} cat = {API.Category} des ={API.Description} link = {API.Link} />
--- src/Datos/Datos.jsx
+++ src/Datos/Datos.jsx
@@ -1,38 +1,18 @@
-import React, { useEffect, useState } from "react";
 
-const Datos = () => {
-  const [dataApi, setDataApi] = useState();
-  const handlerData = async () => {
-    let resp = await fetch("https://api.publicapis.org/entries");
-    resp = await resp.json();
-    setDataApi(resp);
-  };
-  useEffect(() => {
-    handlerData();
-  }, []);
 
-  console.log(dataApi);
+const Datos = ({name, cat, desc, link}) => {
+
 
   return (
     <>
 
-      <h1>Apis info</h1>
-      <div>
-      {
-        dataApi?.entries?.map((API)=>{
-         return(
-
-          <ul >
-          <h3>{API.API}</h3>
-         <li>categoria: {API.Category}</li>
-         <li>descripción: {API.Description}</li>
-         <a href="">{API.Link}</a>
-        </ul>
-         )
-
-        })
-      }
-      </div>
+<div>
+      <h3>{name}</h3>
+      <p>Categoría: {cat}</p>
+      <p>Descripción: {desc}</p>
+      <a href={link}>{link}</a>
+    </div>
+     
 
     </>
   );
